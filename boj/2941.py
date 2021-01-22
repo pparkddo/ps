87ae91ab95833e25cd1743a7d380fb34c3be4237
pparkddo <ehdud3453@gmail.com>
2021-01-23 00:58:58 +0900

    Update

--- boj/2941.py
+++ boj/2941.py
@@ -1,12 +1,3 @@
-def is_pattern_matched(s, patterns):
-    if not s:
-        return False
-    for pattern in patterns:
-        if s in pattern:
-            return True
-    return False
-
-
 patterns = [
     "c=",
     "c-",
@@ -17,28 +8,8 @@ patterns = [
     "s=",
     "z=",
 ]
-maximum_pattern_length = max(map(len, patterns))
 
-count, buffer = 0, []
-for each in input():
-    if "".join([*buffer, each]) in patterns:
-        buffer.clear()
-        count = count + 1
-        continue
-    if is_pattern_matched("".join([*buffer, each]), patterns):
-        buffer.append(each)
-        continue
-    if is_pattern_matched(each, patterns):
-        count = count + len(buffer)
-        buffer.clear()
-        buffer.append(each)
-        continue
-    if len(buffer) == maximum_pattern_length:
-        count = count + len(buffer)
-        buffer.clear()
-        continue
-    count = count + 1 + len(buffer)
-    buffer.clear()
-
-print(count + len(buffer))
-    
+s = input()
+for each in patterns:
+    s = s.replace(each, "_")
+print(len(s))
