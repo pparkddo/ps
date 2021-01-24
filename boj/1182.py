6c14f3fd1829d092680bd7ccc0fa4e065d731a95
pparkddo <ehdud3453@gmail.com>
2021-01-24 18:12:23 +0900

    Update

--- boj/1182.py
+++ boj/1182.py
@@ -1,20 +1,14 @@
+from itertools import combinations
+
+
 n, s = map(int, input().split())
 a = list(map(int, input().split()))
 
-visit = {}  # {node: ps} 
 count = 0
 
-stack = []
-stack.extend([(each,) for each in range(n)])
-
-while stack:
-    node = stack.pop()
-    last_element_index = node[-1]
-    ps = visit.get(node[:-1], 0) + a[last_element_index]
-    visit.update({node: ps})
-    if ps == s:
-        count = count + 1
-    for index in range(last_element_index+1, n):
-        stack.append((*node, index))
+for length in range(1, len(a)+1):
+    for each in combinations(a, length):
+        if sum(each) == s:
+            count = count + 1
 
 print(count)
