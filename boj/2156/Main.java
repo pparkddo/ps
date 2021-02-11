e6cffd25417b548e557e9d9bdae6e8df617e450e
pparkddo <ehdud3453@gmail.com>
2021-02-11 21:07:16 +0900

    Update

--- boj/2156/Main.java
+++ boj/2156/Main.java
@@ -7,28 +7,30 @@ public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
         int n = Integer.parseInt(in.readLine());
-        int[] numbers = new int[n];
+        int[] amounts = new int[n];
         for (int i = 0; i < n; i++) {
-            numbers[i] = Integer.parseInt(in.readLine());
+            amounts[i] = Integer.parseInt(in.readLine());
         }
+        in.close();
         int[] dp = new int[n];
         for (int i = 0; i < n; i++) {
-            int number = numbers[i];
+            int amount = amounts[i];
             if (i == 0) {
-                dp[i] = number;
+                dp[i] = amount;
                 continue;
             }
             if (i == 1) {
-                dp[i] = dp[i-1] + number;
+                dp[i] = amounts[0] + amount;
                 continue;
             }
             if (i == 2) {
-                dp[i] = Math.max(numbers[0], numbers[1]) + number;
+                dp[i] = Math.max(amounts[0], amounts[1]) + amount;
+                dp[i] = Math.max(dp[i], dp[i-1]);
                 continue;
             }
-            dp[i] = Math.max(dp[i-3]+numbers[i-1], dp[i-2]) + number;
+            dp[i] = Math.max(dp[i-2], dp[i-3]+amounts[i-1]) + amount;
+            dp[i] = Math.max(dp[i], dp[i-1]);
         }
         System.out.println(dp[n-1]);
-        in.close();
     }
 }
