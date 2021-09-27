e216d67b2506205f672b70071b30c43bb055bf4d
pparkddo <ehdud3453@gmail.com>
2021-09-28 00:19:50 +0900

    Update

---
+++ programmers/86491/Solution.java
@@ -0,0 +1,17 @@
+class Solution {
+
+    public int solution(int[][] sizes) {
+        int width = 0;
+        int height = 0;
+        for (int[] size : sizes) {
+            if (size[0] > size[1]) {
+                width = Math.max(width, size[0]);
+                height = Math.max(height, size[1]);
+            } else {
+                width = Math.max(width, size[1]);
+                height = Math.max(height, size[0]);
+            }
+        }
+        return width * height;
+    }
+}
