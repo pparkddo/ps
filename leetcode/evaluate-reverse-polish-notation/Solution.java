ea0087f940fc42606846078f59b1906da2117893
pparkddo <ehdud3453@gmail.com>
2024-07-27 14:01:23 +0900

    Update

---
+++ leetcode/evaluate-reverse-polish-notation/Solution.java
@@ -0,0 +1,26 @@
+import java.util.Stack;
+
+class Solution {
+
+    public int evalRPN(String[] tokens) {
+        Stack<Integer> stack = new Stack<>();
+        for (String token : tokens) {
+            if ("+".equals(token)) {
+                stack.push(stack.pop() + stack.pop());
+            } else if ("-".equals(token)) {
+                int a = stack.pop();
+                int b = stack.pop();
+                stack.push(b - a);
+            } else if ("*".equals(token)) {
+                stack.push(stack.pop() * stack.pop());
+            } else if ("/".equals(token)) {
+                int a = stack.pop();
+                int b = stack.pop();
+                stack.push(b / a);
+            } else {
+                stack.push(Integer.parseInt(token));
+            }
+        }
+        return stack.pop();
+    }
+}
