60e99b7c91d08d45b1515c204963dcbde6a2fcbb
pparkddo <ehdud3453@gmail.com>
2024-08-19 22:36:48 +0900

    Update

---
+++ leetcode/candy/Solution.java
@@ -0,0 +1,25 @@
+import java.util.Arrays;
+
+class Solution {
+
+    public int candy(int[] ratings) {
+        int[] candies = new int[ratings.length];
+
+        candies[0] = 1;
+        for (int i = 1; i < ratings.length; i++) {
+            if (ratings[i - 1] < ratings[i]) {
+                candies[i] = candies[i - 1] + 1;
+            } else {
+                candies[i] = 1;
+            }
+        }
+
+        for (int i = ratings.length - 2; i >= 0; i--) {
+            if (ratings[i] > ratings[i + 1]) {
+                candies[i] = Math.max(candies[i], candies[i + 1] + 1);
+            }
+        }
+
+        return Arrays.stream(candies).sum();
+    }
+}
